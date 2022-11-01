9b1a0461ede56541b8286b6b46fa884f77d06faf
Jatin Shrivastava <31873847+jatinshrivastava@users.noreply.github.com>
2022-11-01 07:31:36 -0500

    Initial Commit

---
+++ src/main/java/values/Constants.java
@@ -0,0 +1,28 @@
+package values;
+public class Constants {
+     public static final Integer machine_1_listening_port = 5555;
+     public static final Integer machine_2_listening_port = 6000;
+//     public static final Integer machine_3_listening_port = 4444;
+     public static final Integer participant_listening_port = 9998;
+     public static final Integer coordinator_listening_port = 6000;
+     public static final String localhostURL = "localhost";
+     public static final String CLIENT_COMMIT = "CLIENT_COMMIT";
+     public static final String STATE_REQUEST_VOTE = "REQUEST_VOTE";
+     public static final String STATE_WAITING = "WAITING";
+     public static final String STATE_INIT = "INIT";
+     public static final String  STATE_READY = "READY";
+     public static final String STATE_COMMIT = "COMMIT";
+     public static final String STATE_ABORT = "ABORT";
+     public static final String STATE_GLOBAL_COMMIT = "GLOBAL_COMMIT";
+     public static final String STATE_GLOBAL_ABORT = "GLOBAL_ABORT";
+     public static final String connect = "connect";
+     public static final String disconnect = "disconnect";
+     public static final String message = "message";
+     public static final String type = "type";
+     public static final String sender = "sender";
+     public static final String receiver = "receiver";
+//     public static final String senderVectorClock = "senderVectorClock";
+     public static final String machine1VectorClock = "machine1VectorClock";
+     public static final String machine2VectorClock = "machine2VectorClock";
+     public static final String machine3VectorClock = "machine3VectorClock";
+}
